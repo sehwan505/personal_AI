9e747df53d3d6554f3da6c03a97ecdcf653e882a
sehwan505 <sehwan505@google.com>
2023-05-18 23:14:59 +0900

    fix: some issues

--- personal_ai/ai/openai_api.py
+++ personal_ai/ai/openai_api.py
@@ -1,7 +1,7 @@
 import requests, json
 import openai
 from fastapi import APIRouter
-from .pinecone_api import loading_previous_answers
+from .pinecone_api import appending_shots
 router = APIRouter(
     prefix="/api/openai",
 )
